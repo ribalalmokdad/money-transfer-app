cb7d67e4ec64b6ff019ede1b74614fa0485a1863
Anthony-112 <anthony.g.baaklini@gmail.com>
2025-11-22 13:51:03 +0200

    Implement transaction history API (Task 15)

--- backend/app/Http/Controllers/BeneficiaryController.php
+++ backend/app/Http/Controllers/BeneficiaryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Beneficiary;
 use Illuminate\Http\Request;
 
 class BeneficiaryController extends Controller
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,6 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PaymentMethodController;
 use App\Http\Controllers\BeneficiaryController;
+use App\Http\Controllers\TransactionController;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+
+Route::withoutMiddleware([VerifyCsrfToken::class])->group(function () { 
+
 
 Route::get('/test', function () {
     return view('welcome');
@@ -16,4 +21,9 @@ Route::get('/beneficiaries', [BeneficiaryController::class, 'index']);
 Route::put('/beneficiaries/{id}', [BeneficiaryController::class, 'update']);
 Route::delete('/beneficiaries/{id}', [BeneficiaryController::class, 'destroy']);
 
+// hatayta hek la2an ma 3ana api.php
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/transactions', [TransactionController::class, 'index']);
+});
 
+});
